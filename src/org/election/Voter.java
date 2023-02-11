5f8c51d6c6f3f7725d99baa20bf6b15bdf0329a0
“sathishsrg” <sathishsrgeee@gmail.com>
2023-02-11 19:29:31 +0530

    add

---
+++ src/org/election/Voter.java
@@ -0,0 +1,20 @@
+package org.election;
+
+public class Voter {
+
+	
+	public static void main(String[] args) {
+		int age=17;
+		if (age>=18) {
+			
+			System.out.println("eligible to vote");
+		}	
+			else {
+				
+			
+			System.out.println("not eligible to vote");
+		
+		
+	}
+}
+}
